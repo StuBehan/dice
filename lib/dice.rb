b52387bf1cc7bf6afefe67ac0858e12a6e6be970
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-03-04 15:12:49 +0000

    Co-authored-by: Stu Behan <StuBehan@users.noreply.github.com>

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,23 @@
 class Dice
+  attr_reader :counter, :rolls
+  def initialize
+    @counter = 0
+    @rolls = []
+  end
+
   def roll
-    rand(1..6)
+    num = rand(1..6)
+    @counter += 1
+    @rolls << num
+    return num
+  end
+  def multi_roll(num)
+    dice = []
+    num.times { dice << roll }
+    return dice
+  end
+
+  def score(num)
+    multi_roll(num).sum
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -8,8 +8,20 @@ describe Dice do
     expect(subject.roll).to be_between(1, 6)
   end
   it "return a random number when rolled" do
-    dice = Dice.new
-    allow(dice).to receive(:rand).and_return(5)
-    expect(dice.roll).to eq(5)
+    allow(subject).to receive(:rand).and_return(5)
+    expect(subject.roll).to eq(5)
+  end
+  it "allows rolling specified number of dice" do
+    expect(subject.multi_roll(2).count).to eq(2)
+  end
+  it "allows rolling specified number of dice" do
+    expect(subject.multi_roll(5).count).to eq(5)
+  end
+  it "records all previous die rolls" do
+    expect(subject.rolls.count).to eq(subject.counter)
+  end
+  it "returns sum of multi-dice rolled" do
+    allow(subject).to receive(:rand).and_return(4,2,3,1)
+    expect(subject.score(4)).to eq(10)
   end
 end
